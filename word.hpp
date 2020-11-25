a7407984002078662e8b77628f793d40fa157c00
Ruairí Grant <ruairi.j.grant@gmail.com>
2020-11-25 11:09:10 +0000

    done.

--- word.hpp
+++ word.hpp
@@ -5,5 +5,5 @@ class word : public std::string {
   public:
   word *next; // use this to link to the next  object in the linked list
   int counter = 1;
-  //bool is_in_list(stringlink *head);
+  
 };
